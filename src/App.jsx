9682a9dfb3adafd0008e2bbb977bde417c24533b
= <=>
2024-03-11 16:23:55 +0530

    update: indentation correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-// import './App.css'
-
 function App() {
-  return (
-    <h1>Hello...</h1>
-  )
+    return (
+        <>
+            <h1>Hello...</h1>
+            <div>its exam taking app</div>
+        </>
+    );
 }
 
-export default App
+export default App;
